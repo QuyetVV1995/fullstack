3445aefd59c86a355418326f1fd25ac6bfa83df6
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-26 14:20:03 +0700

    connecting Angular with Get Employee REST API

--- frontend/angular-frontend/src/app/update-employee/update-employee.component.ts
+++ frontend/angular-frontend/src/app/update-employee/update-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-update-employee',
@@ -8,10 +10,18 @@ import { Employee } from '../employee';
 })
 export class UpdateEmployeeComponent implements OnInit {
 
+  id: number;
+
   employee: Employee = new Employee();
-  constructor() { }
+  constructor(private employeeService: EmployeeService,
+     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.employeeService.getEmployeeById(this.id).subscribe(data => {
+      this.employee =data;
+    },
+    error => console.log(error));
   }
 
   onSubmit(){
