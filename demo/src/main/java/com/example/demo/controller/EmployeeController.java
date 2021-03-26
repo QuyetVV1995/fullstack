75eef930337abe97aaf011175b78eeb467c9c7d5
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-26 13:39:56 +0700

    create Rest API to get Single Employee Object

--- demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +17,6 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
@@ -26,4 +27,13 @@ public class EmployeeController {
         return employeeRepository.save(employee);
     }
 
+    // get employee by ID rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        return ResponseEntity.ok(employee);
+    }
 }
