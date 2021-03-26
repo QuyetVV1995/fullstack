3ca721a298ce143851cbc70a85759ef4044deb72
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-26 14:50:07 +0700

    connecting angular with delete employee REST API

--- frontend/angular-frontend/src/app/employee-list/employee-list.component.ts
+++ frontend/angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -34,4 +34,11 @@ export class EmployeeListComponent implements OnInit {
 
   }
 
+  deleteEmployee(id: number){
+    this.employeeService.deleteEmployee(id).subscribe(data => {
+      console.log(data);
+      this.getEmployees();
+    })
+  }
+
 }
