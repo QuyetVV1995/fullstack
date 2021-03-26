1b2aa07abff843dae6d1950e9a3674e5e2eb8801
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-26 14:09:37 +0700

    create angular update employee component & form

--- frontend/angular-frontend/src/app/employee-list/employee-list.component.ts
+++ frontend/angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
 
@@ -12,7 +13,8 @@ export class EmployeeListComponent implements OnInit {
   employees: Employee[];
 
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService,
+    private router: Router) { }
 
   ngOnInit(): void {
 
@@ -25,6 +27,11 @@ export class EmployeeListComponent implements OnInit {
     this.employeeService.getEmployeesList().subscribe(data =>{
       this.employees = data;
     });
+  }
+
+  updateEmployee(id: number){
+    this.router.navigate(['update-employee', id]);
+
   }
 
 }
