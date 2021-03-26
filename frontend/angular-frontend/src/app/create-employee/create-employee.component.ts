6600448d26a397e20dd13e274142253c5ae9ce84
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-26 11:58:40 +0700

    Connecting Angular with create employee REST API

--- frontend/angular-frontend/src/app/create-employee/create-employee.component.ts
+++ frontend/angular-frontend/src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,8 @@
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-create-employee',
@@ -10,12 +13,27 @@ export class CreateEmployeeComponent implements OnInit {
 
   employee: Employee = new Employee();
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService,
+      private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  saveEmployee(){
+    this.employeeService.createEmployee(this.employee).subscribe(data =>{
+      console.log(data);
+      this.gotoEmployeeList();
+    }),
+   error => console.log(error);
+
+  }
+
+  gotoEmployeeList(){
+    this.router.navigate(['/employees']);
+  }
+
   onSubmit(){
     console.log(this.employee);
+    this.saveEmployee();
   }
 }
