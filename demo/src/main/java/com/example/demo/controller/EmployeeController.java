a1c6c554c3468ed970d8a333304290b4bf1e3722
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-26 13:49:28 +0700

    creating Update Employee REST API

--- demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -33,7 +33,19 @@ public class EmployeeController {
 
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
-
         return ResponseEntity.ok(employee);
+    }
+
+    // update employee API
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id,@RequestBody Employee employeeDetails){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+
+        Employee updateEmployee =  employeeRepository.save(employee);
+        return ResponseEntity.ok(updateEmployee);
     }
 }
