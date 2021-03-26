cef8ad9d354d54860c6397f8cbdecf5a40309de8
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-26 11:21:49 +0700

    creating angular create employee component

--- frontend/angular-frontend/src/app/app.module.ts
+++ frontend/angular-frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { from } from 'rxjs';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
